d18120d0d2165b9553a2f09212fdb491e09c3944
Nawalhossam <nawalhossam11116@gmail.com>
2023-12-26 23:23:48 +0200

    edit

--- pickbyboth.cpp
+++ pickbyboth.cpp
@@ -149,14 +149,14 @@ void pickbyboth::Execute()
 						correct++;
 						pManager->UpdateInterface();
 				
-						pOut->PrintMessage("you are right continue is " + to_string(correct) + "the incorrect anwsers" + to_string(incorrect)+ " choose "+(arrofquestions[random]) );
+						pOut->PrintMessage("you are right continue is " + to_string(correct) + " the incorrect anwsers " + to_string(incorrect)+ " choose "+(arrofquestions[random]) );
 					
 						
 					}
 					else  if (pManager->insideofboth(p1, random) == 0)
 					{
 						incorrect++;
-						pOut->PrintMessage("this is the false anwser " + (arrofquestions[random]) + " the correct is " + to_string(correct) + "the incorrect anwsers" + to_string(incorrect)+" please choose "+(arrofquestions[random]));
+						pOut->PrintMessage("this is the false anwser " + (arrofquestions[random]) + " the correct is " + to_string(correct) + " the incorrect anwsers " + to_string(incorrect)+" please choose "+(arrofquestions[random]));
 					}
 					else if (pManager->insideofboth(p1, random) == -1)
 						pOut->PrintMessage("please click on a fig");
@@ -169,11 +169,11 @@ void pickbyboth::Execute()
 		
 		if (i == sum)
 		{
-			pOut->PrintMessage("please click toget the final score");
+			pOut->PrintMessage("please click to get the final score");
 			pIn->GetPointClicked(p1.x, p1.y);
 			pManager->unhide();
 			pManager->UpdateInterface();
-			pOut->PrintMessage("the final correct anwsers" + to_string(correct) + "the final incorrect anwsers" + to_string(incorrect));
+			pOut->PrintMessage("the final correct anwsers " + to_string(correct) + " the final incorrect anwsers " + to_string(incorrect));
 		}
 	//	pAct = NULL;
 
